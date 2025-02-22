193b0d6db7e63aa889413d8212bec620283c52fb
rgweh <netonetonetot@gmail.com>
2025-02-22 20:43:22 +0700

    fix 2
    Refix of first remarks

--- Assets/Counter.cs
+++ Assets/Counter.cs
@@ -9,31 +9,36 @@ public class Counter : MonoBehaviour
     private int _number = 0;
     private bool _isWorking = false;
 
+    private void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
     private void OnMouseUp()
     {
         Debug.Log("pressed");
 
         if (_isWorking)
-        {
             _isWorking = false;
-        }
         else
-        {
             _isWorking = true;
-            StartCoroutine(Countdown());
-        }
     }
 
     private IEnumerator Countdown()
     {
         var wait = new WaitForSeconds(_counterSpeed);
 
-        for (int i = 0; _isWorking; i++)
+        for (int i = 0; i >= 0; i++)
         {
-            _number++;
-            _counterUpdater.UpdateCounter(_number);
+            if (_isWorking)
+            {
+                _number++;
+                _counterUpdater.UpdateCounter(_number);
 
-            yield return wait;
+                yield return wait;
+            }
+            else
+                yield return null;
         }
     }
 }
